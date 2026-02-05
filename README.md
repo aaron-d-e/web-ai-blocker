c966cd5a4d2d2b995095a7d281c583404d88381e
Aaron Evans <aarone0621@gmail.com>
2026-02-04 21:35:32 -0600

    memes and stuff

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-# Web Extension AI-blocker
+# AI web blocker extension - "Monkey Lock"
 
-- need to have a window to turn on the extenstion
-- blocks AI overview and urls (chatgpt, claude, gemini, etc)
-- timer to show how "locked in" youve been (timer that tracks the time youve had the extension on)
+![Monkey Lock](images/monkey8.png)
+
+I found myself accepting the first answer thrown at me when looking up something.This led to an increase in imposter syndrome and wanted something to remove the temptation. This extension is a silly little remedy to that. It blocks the AI overview from appearing on you browser when you search. It also blocks all mainstream AI chat websites and feeds you a monkey meme, locking you out of the site (Monkey Lock). To turn it off, theres a toggle switch in the extension popup, but you should feel dirty and gross as you turn slide that switch. Instead of accepting any answer that AI shoves at you, laugh at a monkey meme and read the damn docs. 
+
+## Currently blocks access to:
+- ChatGPT
+- Gemini
+- Google AI
+- OpenAI
+- Co-pilot
+- Claude
+- Perplexity (currently having problems getting this one to work for some reason)
 
--- blocked.js
+++ blocked.js
@@ -1,17 +1,41 @@
 const images = [
-  "/images/monkey-i-see.gif",
+  "/images/monkey1.webp",
+  "/images/monkey2.webp",
+  "/images/monkey3.jpg",
+  "/images/monkey4.webp",
+  "/images/monkey5.jpg",
+  "/images/monkey6.jpg",
+  "/images/monkey7.webp",
+  "/images/monkey8.gif",
+  "/images/monkey9.jpg",
+  "/images/monkey10.gif",
+  "/images/monkey10.jpg",
+  "/images/monkey11.webp",
+  "/images/monkey12.webp"
+]
+
+const text = [
+  "You promised you wouldn't...",
+  "You've been a naughty monkey...",
+  "You're a bad, bad monkey...",
+  "Gotcha!",
+  "Go read an article bro...",
+  "What do you think you're doing?"
 ]
 const container = document.getElementById("container"); //insert html element into container
 
 function showImage() {
   console.log("function ran from blocked.js");
-  let size = images.length;
+  let images_size = images.length;
+  let text_size = text.length;
 
-  let rand = Math.floor((Math.random() * 100)) % size;
+  let rand = Math.floor((Math.random() * 100)) % images_size;
+  let rand_text = Math.floor((Math.random() * 100)) % text_size;
   console.log("Random Value is: " + rand);
 
   let getImg = "";
   getImg += `<img class="blocked-img" src="${images[rand]}">` //blocked img in blocked.css
+  getImg += `<p class="monkey-text">${text[rand_text]}</p>`;
 
   container.innerHTML = getImg
 }
